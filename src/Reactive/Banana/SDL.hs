12131a7bbcdd38009721a8e1379e841e4e4bf9e9
R. Kyle Murphy <orclev@gmail.com>
2012-04-04 00:24:07 -0400

    Have things mostly working.

--- src/Reactive/Banana/SDL.hs
+++ src/Reactive/Banana/SDL.hs
@@ -1,13 +1,20 @@
-module Reactive.Banana.SDL where
+module Reactive.Banana.SDL ( EventSource, SDLEventSource, WrappedEvent, TickEvent
+                           , addHandler, fire, getSDLEventSource, sdlEvent
+                           , tickEvent, keyEvent, mouseEvent, mouseButtonEvent
+                           , filterEq, runSDLPump, runCappedSDLPump, fpsEvent ) where
 
+import Control.Monad
 import Reactive.Banana as R
 import Graphics.UI.SDL as SDL
 import qualified Graphics.UI.SDL.Time as SdlTime
 import Data.Word
 
 type EventSource a = (AddHandler a, a -> IO ())
-type SDLEventSource = (EventSource SDL.Event, EventSource Word32)
 type WrappedEvent t = R.Event t SDL.Event
+type TickEvent t = R.Event t Word32
+data SDLEventSource = SDLEventSource { getSDLEvent :: EventSource SDL.Event
+                                     , getTickEvent :: EventSource Word32
+                                     , getFPSEvent :: EventSource Word32 }
 
 addHandler :: EventSource a -> AddHandler a
 addHandler = fst
@@ -16,13 +23,16 @@ fire :: EventSource a -> a -> IO ()
 fire = snd
 
 getSDLEventSource :: IO SDLEventSource
-getSDLEventSource = (,) <$> newAddHandler <*> newAddHandler
+getSDLEventSource = SDLEventSource <$> newAddHandler <*> newAddHandler <*> newAddHandler
 
 sdlEvent :: SDLEventSource -> NetworkDescription t (WrappedEvent t)
-sdlEvent = fromAddHandler . addHandler . fst
+sdlEvent = fromAddHandler . addHandler . getSDLEvent
 
-tickEvent :: SDLEventSource -> NetworkDescription t (R.Event t Word32)
-tickEvent = fromAddHandler . addHandler . snd
+tickEvent :: SDLEventSource -> NetworkDescription t (TickEvent t)
+tickEvent = fromAddHandler . addHandler . getTickEvent
+
+fpsEvent :: SDLEventSource -> NetworkDescription t (TickEvent t)
+fpsEvent = fromAddHandler . addHandler . getFPSEvent
 
 keyEvent :: WrappedEvent t -> WrappedEvent t
 keyEvent = filterE isKey
@@ -48,14 +58,46 @@ mouseButtonEvent = filterE isButton
             MouseButtonUp _ _ _ -> True
             otherwise -> False
 
-runSDLPump :: SDLEventSource -> IO ()
-runSDLPump es = do
-    let esdl = fst es
-        etick = snd es
+filterEq :: Eq a => R.Event t a -> R.Event t a
+filterEq = filterJust . fst . mapAccum Nothing . fmap f
+    where
+        f y (Just x) = if x == y then (Nothing, Just x) else (Just y, Just y)
+        f y Nothing  = (Just y, Just y)
+
+mainSDLPump :: SDLEventSource -> IO Bool
+mainSDLPump es = do
+    let esdl = getSDLEvent es
+        etick = getTickEvent es
     e <- pollEvent
     tick <- SdlTime.getTicks
     case e of
-        Quit -> return ()
-        otherwise -> fire esdl e
-    fire etick tick
-    runSDLPump es
+        Quit -> return False
+        otherwise -> do
+            fire esdl e
+            fire etick tick
+            return True
+
+runSDLPump :: SDLEventSource -> IO ()
+runSDLPump es = do
+    startTick <- SdlTime.getTicks
+    c <- mainSDLPump es
+    endTick <- SdlTime.getTicks
+    let ticks = fromIntegral (endTick - startTick)
+        fps = 1000 `div` (max ticks 1)
+        efps = getFPSEvent es
+    fire efps fps
+    if c then runSDLPump es else return ()
+
+runCappedSDLPump :: Int -> SDLEventSource -> IO ()
+runCappedSDLPump rate es = do
+    startTick <- SdlTime.getTicks
+    c <- mainSDLPump es
+    endTick <- SdlTime.getTicks
+    let ticks = fromIntegral (endTick - startTick)
+        secsPerFrame = fromIntegral (1000 `div` rate)
+        fps = min (1000 `div` (max ticks 1)) (fromIntegral rate)
+        efps = getFPSEvent es
+    fire efps fps
+    when (ticks < secsPerFrame) $ do
+        delay $ secsPerFrame - ticks
+    if c then runCappedSDLPump rate es else return ()
