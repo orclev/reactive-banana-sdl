2a4fbd770575ba4c51012dca4fd40a46be544664
R. Kyle Murphy <orclev@gmail.com>
2012-04-23 01:58:35 -0400

    Work continues on my refactor.

--- src/Reactive/Banana/SDL/Graphics/Types.hs
+++ src/Reactive/Banana/SDL/Graphics/Types.hs
@@ -10,21 +10,19 @@ import Data.Lens.Template
 
 type Screen = SDL.Surface
 newtype Graphic = Graphic { paintGraphic :: Screen -> IO () }
-data Mask = M { maskClip :: Maybe Rect, maskX :: Int, maskY :: Int }
+type GraphicOpt = Rect -> Graphic
+type GraphicUpdate = (GraphicOpt, Rect)
+data Mask = Mask { _maskClip :: Maybe Rect, _maskX :: Int, _maskY :: Int }
+data Fill = Fill { _fillClip :: Maybe Rect, _fillColor :: Color }
+data Text = Text { _textMsg :: String, _textFont :: Font, _textColor :: Color }
+
+$(makeLenses [''Mask, ''Fill, ''Text])
 
 instance Eq Color where
     (Color r1 g1 b1) == (Color r2 g2 b2) = r1 == r2 && g1 == g2 && b1 == b2
 
 instance Show Color where
     show (Color r g b) = "Color { " ++ show r ++ ", " ++ show g ++ ", " ++ show b ++ " }"
-
-data Widget = FileImage { _imagePath :: FilePath } |
-    Image { _imageData :: SDL.Surface, _imageClip :: Maybe Rect } | Fill { _fillColor :: Color } |
-    Text { _textColor :: Color, _textMessage :: String, _textFont :: Font } |
-    Over { _topItem :: Widget, _bottomItem :: Widget }
-    deriving (Eq, Show)
-
-$(makeLenses [''Widget])
 
 class Draw canvas mask where
     draw :: canvas -> mask -> Graphic
--- src/Reactive/Banana/SDL/Graphics/Util.hs
+++ src/Reactive/Banana/SDL/Graphics/Util.hs
@@ -16,35 +16,61 @@ over :: Graphic -> Graphic -> Graphic
 under :: Graphic -> Graphic -> Graphic
 under = flip over
 
-empty :: Graphic
-empty = Graphic $ \_ -> return ()
+emptyG :: Graphic
+emptyG = Graphic $ \_ -> return ()
 
 render :: Graphic -> Graphic
 render (Graphic x) = Graphic $ \surface -> x surface >> SDL.flip surface
 
+withinBox :: Rect -> Graphic -> GraphicOpt
+withinBox r g = \r' ->
+    if r `intersect` r' then emptyG else g
+
+overOpt :: GraphicOpt -> GraphicOpt -> GraphicOpt
+overOpt g1 g2 = \r -> g1 r `over` g2 r
+
+overUpdate :: GraphicOpt -> GraphicUpdate -> GraphicUpdate
+overUpdate g1 (g2,r) = (g1 `overOpt` g2, r)
+
+intersect :: Rect -> Rect -> Bool
+intersect r1 r2 = xintersect && yintersect
+    where
+        xintersect = x1 `between` (x2, w2) || w1 `between` (x2,w2)
+        yintersect = y1 `between` (y2,h2) || h1 `between` (y2,h2)
+        x1 = rectX r1
+        x2 = rectX r2
+        y1 = rectY r1
+        y2 = rectY r2
+        w1 = x1 + rectW r1
+        w2 = x2 + rectW r2
+        h1 = y1 + rectH r1
+        h2 = y2 + rectH r2
+
+between :: Int -> (Int,Int) -> Bool
+between x (l,h) = x >= l && x <= h
+
 instance Draw SDL.Surface Mask where
     draw src mask = Graphic $ \dst -> discard $ blitSurface src clip dst offset
         where
-            clip = maskClip mask
-            offset = Just $ Rect { rectX = maskX mask, rectY = maskY mask, rectW = 0, rectH = 0 }
+            clip = maskClip ^$ mask
+            offset = Just $ Rect { rectX = maskX ^$ mask, rectY = maskY ^$ mask, rectW = 0, rectH = 0 }
 
-instance Draw Widget Mask where
-    draw widget mask = Graphic $ \dst -> renderWidget widget dst mask
+instance Draw Fill Mask where
+    draw fill mask = Graphic $ \dst -> pixel dst >>= \c -> discard $ fillRect dst clip c
+        where
+            pixel dst = (mapRGB . surfaceGetPixelFormat) dst (colorRed color) (colorGreen color) (colorBlue color)
+            clip = fillClip ^$ fill
+            color = fillColor ^$ fill
 
-renderWidget :: Widget -> Surface -> Mask -> IO ()
-renderWidget w dst mask = case w of
-    Over _ _ -> renderWidget (w ^. bottomItem) dst mask >> (discard $ renderWidget (w ^. topItem) dst mask)
-    Fill _ -> pixel dst >>= \c -> discard $ fillRect dst Nothing c
-    Image _ _ -> discard $ blitSurface (w ^. imageData) (w ^. imageClip) dst offset
-    Text _ _ _ -> discard $ blitText dst
-    where
-        blitText dst = do
-            txt <- renderTextSolid (w ^. textFont) (w ^. textMessage) (w ^. textColor)
-            blitSurface txt (maskClip mask) dst offset
-            freeSurface txt
-        offset = Just $ Rect { rectX = maskX mask, rectY = maskY mask, rectW = 0, rectH = 0 }
-        pixel dst = (mapRGB . surfaceGetPixelFormat) dst (colorRed color) (colorGreen color) (colorBlue color)
-        color = w ^. fillColor
+instance Draw Text Mask where
+    draw text mask = Graphic $ \dst -> discard $ blitText dst
+        where
+            blitText dst = do
+                txt <- renderTextSolid (textFont ^$ text) (textMsg ^$ text) (textColor ^$ text)
+                blitSurface txt clip dst offset
+                freeSurface txt
+            clip = maskClip ^$ mask
+            offset = Just $ Rect { rectX = maskX ^$ mask, rectY = maskY ^$ mask, rectW = 0, rectH = 0 }
 
 discard :: IO a -> IO ()
 discard m = m >> return ()
